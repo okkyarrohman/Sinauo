64610bc299d0735f18fdb24dd036a6990cdc76e8
okkyarrohman <okkyzee@gmail.com>
2024-01-15 12:10:47 +0700

    Add user_id on Submateri Table

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Hasil::class);
     }
+
+    public function submateri()
+    {
+        return $this->hasMany(Submateri::class);
+    }
 }

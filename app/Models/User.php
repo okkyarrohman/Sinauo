5bfc6997eaef1be1a183e65aed600e1a0be6b73e
okkyarrohman <okkyzee@gmail.com>
2024-01-15 12:18:38 +0700

    Add 4 Column in User tabel
    tempat lahir, alamat, foto, telepon

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'tempat_lahir',
+        'alamat',
+        'foto',
+        'telepon'
     ];
 
     /**
